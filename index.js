eec29ec70e105d63097d9a596354d9b3271642c6
Juan Batiz-Benet <juan@benet.ai>
2014-06-12 04:01:45 -0700

    new version

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var transformer = require('dat-transformer');
-var tXmlString = transformer('xml-string');
-var tJsonml = transformer('jsonml');
+var Conversion = require('transformer-conversion');
+var tXmlString = require('transformer.xml-string');
+var tJsonml = require('transformer.jsonml');
 // require any other modules you may need here.
 var jsxml = require('jsxml');
 
-module.exports = transformer.Conversion(tXmlString, tJsonml, convert);
+module.exports = Conversion(tXmlString, tJsonml, convert);
 
 function convert(xml) {
   return jsxml.fromXml(xml)[0];
